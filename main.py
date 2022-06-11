d34e1957bcf500ef1c5714292b67693fb7cdb0b6
Teoh Kheng Hong <tkhenghong@gmail.com>
2022-06-11 15:02:37 +0800

    Initial spelling checking logic, pending recommended words search box logic

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from tkinter.scrolledtext import ScrolledText
 from tkmacosx import Button
 from spellchecker import SpellChecker
 
-spell = SpellChecker()
+spell = SpellChecker(language='en')
+
+current_text = 'let us wlak on the groun'
+current_wrong_word = ''
+recommended_words = []
+recommended_words_search_result = []
 
 root = Tk()
 root.title('NLP Demonstration: Spelling Correction')
@@ -12,56 +17,62 @@ root.columnconfigure(1, weight=1)
 
 
 def analyze_text():
-    text = user_input_textbox.get('1.0', END)
-    print('text: ', text)
-    # SAMPLE TEXT: Let us wlak on the groun
-    split_text = text.split()
-    print('split_text: ', split_text)
+    global current_text
+    global current_wrong_word
+    current_text = user_input_textbox.get('1.0', END)
+    split_text = current_text.split()
 
     misspelled = spell.unknown(split_text)
-    print('type(misspelled): ', type(misspelled))
-    print('misspelled: ', misspelled)
 
+    clear_recommended_words()
     if len(misspelled) > 0:
-        print('misspelled: ', misspelled)
-        show_recommended_words(list(misspelled)[0])
-        mark_red_text()
-
-    # user_input_textbox.delete(1.0, 'end')
-    # for original_word in split_text:
-    #     if original_word in misspelled:
-    #         # TODO: mark the word in the textbox
-    #         mark_red_text()
-    #         corrected_word = spell.correction(original_word)
-    #         print('corrected_word: ', corrected_word)
-    #         user_input_textbox.insert(END, corrected_word)
-    #         show_recommended_words(original_word)
-    #         break
-
+        current_wrong_word = list(misspelled)[0]
+        show_recommended_words(current_wrong_word)
     user_input_textbox.focus_set()
 
 
-def mark_red_text():
-    user_input_textbox.tag_add('red', '1.0', 'end')
-    user_input_textbox.tag_config('red', foreground='red')
+def on_select(event):
+    print('on_select')
+    selected_item = listbox.get(ANCHOR)
+    print('selected_item: ', selected_item)
+    correct_misspelled_word(selected_item)
 
 
-def clear_tags():
-    user_input_textbox.tag_remove('red', '1.0', 'end')
+def clear_recommended_words():
+    misspelled_word_label.config(text='Misspelled word: None')
+    listbox.delete(0, END)
+    listbox.unbind('<Double-1>')
 
 
 def show_recommended_words(wrong_word):
+    global recommended_words
     misspelled_word_label.config(text='Misspelled word: ' + wrong_word)
     recommended_words = spell.candidates(wrong_word)
     print('recommended_words: ', recommended_words)
-    listbox.delete(0, END)
     for word in recommended_words:
         listbox.insert(END, word)
+    listbox.bind('<Double-1>', on_select)
 
 
-def search_word():
-    # TODO: search the word in the dictionary
-    print('search_word')
+def correct_misspelled_word(corrected_word):
+    global current_text
+    global current_wrong_word
+    user_input_textbox.delete(1.0, 'end')
+    split_text = current_text.split()
+
+    for i in range(len(split_text)):
+        if split_text[i] in current_wrong_word:
+            split_text[i] = corrected_word
+            break
+
+    new_sentence = ' '.join(split_text)
+    user_input_textbox.insert(END, new_sentence)
+    analyze_text()  # Analyze again
+
+
+def search_word(event):
+    # TODO:
+
     return
 
 
@@ -95,6 +106,7 @@ user_input_label = Label(user_input, text='Input:')
 user_input_label.grid(column=0, row=0, sticky='nw')
 user_input_textbox = ScrolledText(user_input)
 user_input_textbox.grid(column=0, row=1, sticky='s')
+user_input_textbox.insert(END, current_text)
 user_input_textbox.focus_set()
 
 user_input.grid(column=0, row=0, padx=5, pady=5)
@@ -111,7 +123,7 @@ scrollbar = Scrollbar(right_panel, orient="vertical")
 listbox = Listbox(right_panel, activestyle='dotbox', yscrollcommand=scrollbar.set)
 
 misspelled_word_label.grid(column=0, row=0, sticky='n')
-listbox.grid(column=0, row=1, sticky='new', padx=10, pady=5)
+listbox.grid(column=0, row=1, sticky='news', padx=10)
 scrollbar.grid(column=0, row=1, sticky='nse')
 
 # Search bar
@@ -123,6 +135,7 @@ search_bar.columnconfigure(0, weight=1)
 search_label = Label(search_bar, text='Search:')
 search_input = Text(search_bar, height=1)
 search_button = Button(search_bar, text='Search', command=search_word)
+search_input.bind("<Key>", search_word)
 
 search_label.grid(column=0, row=0, sticky='se')
 search_input.grid(column=1, row=0, sticky='ws', padx=5, pady=5)
