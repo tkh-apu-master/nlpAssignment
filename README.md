23057966215bb4429b81722c3969613eb40fb854
Teoh Kheng Hong <tkhenghong@gmail.com>
2022-06-17 22:56:06 +0800

    Fixed search algorithm, spell checking main algorithm complete

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # NLP Group Assignment
 
+## Main libraries used:
+* [PySpellChecker](https://github.com/barrust/pyspellchecker)
+* 
+
+## Primary techniques used:
+* Peter Norvig's [Levenshtein Distance](https://en.wikipedia.org/wiki/Levenshtein_distance) algorithm
+* Frequency Distribution
+* [N-Gram](https://en.wikipedia.org/wiki/N-gram)
+
 # Quick Guide
 # Steps
 
 * pip install tkmacosx (for mac)
-* pip install pyspellchecker
+* pip install pyspellchecker (DONt use this command)
+* git clone https://github.com/barrust/pyspellchecker.git
+* cd pyspellchecker
+* python setup.py install
 * python -m pip install -U symspellpy
--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 from tkinter import *
 from tkinter.scrolledtext import ScrolledText
+import re
+import pkg_resources
+
 from tkmacosx import Button
 from spellchecker import SpellChecker
+from symspellpy import SymSpell
 
-spell = SpellChecker(language='en')
+spell = SpellChecker()
 
-current_text = 'let us wlak on the groun'
+sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
+
+dictionary_path = pkg_resources.resource_filename("symspellpy", "frequency_dictionary_en_82_765.txt")
+bigram_path = pkg_resources.resource_filename("symspellpy", "frequency_bigramdictionary_en_243_342.txt")
+
+sym_spell.load_dictionary(dictionary_path, term_index=0, count_index=1)
+sym_spell.load_bigram_dictionary(bigram_path, term_index=0, count_index=2)
+
+current_sentence = ''
+
+# Sample wrong sentences
+# current_sentence = 'let us wlak on the groun. it\'s weird that...'
+# current_sentence = 'It is a truth unіversally acknowledged, that a single man in possesssion of a good fortune must be' \
+#                    ' in want of a wife. However little known the feelings or views of such a man may be on his first' \
+#                    ' entering a neighbourhood, this truth is so well fixed in the minds of the surrounding families,' \
+#                    ' that he is considered as the rightful property of some one or other of their daugters.'
 current_wrong_word = ''
 recommended_words = []
 recommended_words_search_result = []
@@ -16,29 +35,70 @@ root.rowconfigure(0, weight=1)
 root.columnconfigure(1, weight=1)
 
 
-def analyze_text():
-    global current_text
-    global current_wrong_word
-    current_text = user_input_textbox.get('1.0', END)
-    split_text = current_text.split()
+def submit():
+    global current_sentence
+    current_sentence = user_input_textbox.get('1.0', END)
 
+    require_check = check_sentence()
+
+    if require_check:
+        return
+        # TODO: Suggest correct sentence spacing
+    else:
+        analyze_text()
+
+    user_input_textbox.focus_set()
+
+
+# TODO: Analyze sentence and suggest correct spacing
+def check_sentence():
+    # print('Check using SymSpell: ')
+    # print('When checking as a sentence: ')
+    # suggestions = sym_spell.lookup_compound(current_sentence, max_edit_distance=2)
+    # print('suggestions: ', suggestions)
+    # for suggestion in suggestions:
+    #     print('Sentence suggestion: ', suggestion)
+    # print('When checking word by word: ')
+    # for word in split_text:
+    #     suggestions = sym_spell.lookup_compound(word, max_edit_distance=2)
+    #     print('suggestions: ', suggestions)
+    #     for suggestion in suggestions:
+    #         print(word, ' suggestion: ', suggestion)
+    return False
+
+
+def analyze_text():
+    global current_sentence
+    global current_wrong_word
+    # Split sentence into words precisely using regex: https://stackoverflow.com/a/26209841
+    split_text = re.findall(r"(?<![@#])\b\w+(?:'\w+)?", current_sentence)
     misspelled = spell.unknown(split_text)
 
     clear_recommended_words()
     if len(misspelled) > 0:
         current_wrong_word = list(misspelled)[0]
         show_recommended_words(current_wrong_word)
+
+
+def clear_input():
+    user_input_textbox.delete(1.0, 'end')
     user_input_textbox.focus_set()
 
 
 def on_select(event):
-    print('on_select')
     selected_item = listbox.get(ANCHOR)
-    print('selected_item: ', selected_item)
     correct_misspelled_word(selected_item)
+    submit()  # Analyze again
+
+
+def clear():
+    clear_input()
+    clear_recommended_words()
 
 
 def clear_recommended_words():
+    global recommended_words_search_result
+    recommended_words_search_result.clear()
     misspelled_word_label.config(text='Misspelled word: None')
     listbox.delete(0, END)
     listbox.unbind('<Double-1>')
@@ -47,33 +107,45 @@ def clear_recommended_words():
 def show_recommended_words(wrong_word):
     global recommended_words
     misspelled_word_label.config(text='Misspelled word: ' + wrong_word)
-    recommended_words = spell.candidates(wrong_word)
-    print('recommended_words: ', recommended_words)
+    recommended_words = list(spell.candidates(wrong_word))
     for word in recommended_words:
         listbox.insert(END, word)
     listbox.bind('<Double-1>', on_select)
 
 
 def correct_misspelled_word(corrected_word):
-    global current_text
+    global current_sentence
     global current_wrong_word
     user_input_textbox.delete(1.0, 'end')
-    split_text = current_text.split()
+    split_text = current_sentence.split()
 
     for i in range(len(split_text)):
         if split_text[i] in current_wrong_word:
             split_text[i] = corrected_word
             break
 
-    new_sentence = ' '.join(split_text)
-    user_input_textbox.insert(END, new_sentence)
-    analyze_text()  # Analyze again
+    current_sentence = current_sentence.replace(current_wrong_word, corrected_word)
+    user_input_textbox.insert(END, current_sentence)
 
 
 def search_word(event):
-    # TODO:
+    global recommended_words_search_result
+    listbox.delete(0, END)
+    listbox.unbind('<Double-1>')
+    recommended_words_search_result.clear()
+    search_keyword = search_input.get(1.0, 'end')
 
-    return
+    for i in range(len(recommended_words)):
+        if search_keyword in recommended_words[i]:
+            recommended_words_search_result.append(recommended_words[i])
+
+    # If search_keyword is empty, show all recommended words as default
+    if len(search_keyword) == 1:
+        recommended_words_search_result.extend(recommended_words)
+
+    for word in recommended_words_search_result:
+        listbox.insert(END, word)
+    listbox.bind('<Double-1>', on_select)
 
 
 def close():
@@ -92,10 +164,12 @@ frm_buttons.rowconfigure(0, weight=1)
 frm_buttons.columnconfigure(0, weight=1)
 frm_buttons.grid(column=0, row=1, sticky='nsew')
 
-btn_submit = Button(frm_buttons, text='Submit', command=analyze_text, background='#00CA4E', foreground='white')
-btn_exit = Button(frm_buttons, text='Exit', command=close, background='#FF605C', foreground='white')
+btn_submit = Button(frm_buttons, text='Submit', command=submit, background='#00CA4E', foreground='black')
+btn_clear = Button(frm_buttons, text='Clear', command=clear, background='#FFBD44', foreground='black')
+btn_exit = Button(frm_buttons, text='Exit', command=close, background='#FF605C', foreground='black')
 btn_submit.grid(column=0, row=1, sticky='nsew')
-btn_exit.grid(column=0, row=2, sticky='nsew')
+btn_clear.grid(column=0, row=2, sticky='nsew')
+btn_exit.grid(column=0, row=3, sticky='nsew')
 
 # User input
 user_input = Frame(left_panel, relief=RAISED)
@@ -106,7 +180,7 @@ user_input_label = Label(user_input, text='Input:')
 user_input_label.grid(column=0, row=0, sticky='nw')
 user_input_textbox = ScrolledText(user_input)
 user_input_textbox.grid(column=0, row=1, sticky='s')
-user_input_textbox.insert(END, current_text)
+user_input_textbox.insert(END, current_sentence)
 user_input_textbox.focus_set()
 
 user_input.grid(column=0, row=0, padx=5, pady=5)
@@ -134,11 +208,9 @@ search_bar.columnconfigure(0, weight=1)
 
 search_label = Label(search_bar, text='Search:')
 search_input = Text(search_bar, height=1)
-search_button = Button(search_bar, text='Search', command=search_word)
-search_input.bind("<Key>", search_word)
+search_input.bind("<KeyRelease>", search_word)
 
 search_label.grid(column=0, row=0, sticky='se')
-search_input.grid(column=1, row=0, sticky='ws', padx=5, pady=5)
-search_button.grid(column=2, row=0, sticky='se')
+search_input.grid(column=1, row=0, sticky='ws', padx=10, pady=5)
 
 root.mainloop()
